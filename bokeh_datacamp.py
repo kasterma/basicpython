d32a8df8aac49215e3a8f7c627a0b92da4aa6a97
Bart Kastermans <kasterma@kasterma.net>
2018-12-22 18:40:37 +0100

    bokeh ColumnDataSource

--- bokeh_datacamp.py
+++ bokeh_datacamp.py
@@ -7,5 +7,13 @@ plot = figure(plot_width=600, plot_height=400, tools="pan, box_zoom", x_axis_lab
 plot.circle([1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 1, 2])
 plot.x(x=[1, 2, 3], y=[6, 7, 8], size=20)
 plot.annulus(x=[2, 3, 4], y=[5, 6, 7], inner_radius=0.1, outer_radius=0.5)
+
+
+from bokeh.models import ColumnDataSource
+
+source = ColumnDataSource(data={'aaa':  [1, 2, 3, 4], 'bbb': [1, 2, 1, 5]})
+p2 = figure(plot_width=600, plot_height=400, tools="pan, box_zoom", x_axis_label='aaa-axis', y_axis_label='bbb-axis')
+p2.circle('aaa', 'bbb', source=source)
+
 output_file("circle.html")
-show(plot)
+show(p2)
