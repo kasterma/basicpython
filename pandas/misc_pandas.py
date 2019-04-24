7de3ef65a87e251e0a701a7292e02d5dfedad0ab
Bart Kastermans <kasterma@kasterma.net>
2019-04-24 10:19:58 +0200

    wip

--- pandas/misc_pandas.py
+++ pandas/misc_pandas.py
@@ -9,7 +9,12 @@ x3.loc[2, ['b']]
 x3.iloc[[2]]
 x3.loc[[1]]
 
-x3.loc[1]     # this is a Series
-x3.loc[2]     # this is a DataFrame
+x3.loc[1]  # this is a Series
+x3.loc[2]  # this is a DataFrame
 x3.loc[[1]]
-x3.loc[[2]]   # are both dataframes.
+x3.loc[[2]]  # are both dataframes.
+
+d1 = pd.DataFrame({'a': [1, 2, 3]})
+d1['b'] = d1['a'] * 11
+d2 = pd.DataFrame({'a': [2, 3, 4]})
+d2['c'] = d2['a'] * 11
