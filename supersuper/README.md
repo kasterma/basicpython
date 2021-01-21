8e48b70fb40665b875207255024f826f5fbef522
Bart Kastermans <kasterma@kasterma.net>
2021-01-21 16:21:34 +0100

    feat(supersuper): ex1

---
+++ supersuper/README.md
@@ -0,0 +1,5 @@
+# super super
+
+Improving my super understanding.  Prepping for some more library design.
+
+[1] https://rhettinger.wordpress.com/2011/05/26/super-considered-super/
---
+++ supersuper/ex1.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+log = logging.getLogger("ex1")
+
+
+class LoggingDict(dict):
+    def __setitem__(self, key, value):
+        log.info("Setting %r to %r" % (key, value))
+        super().__setitem__(key, value)
+
+
+l = LoggingDict()
+l[1] = 2
